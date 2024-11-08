a46990ba37be63cdce97383b043c43fda21a9fc7
Abolfazl <abtarahomii@gmail.com>
2024-11-08 12:27:54 +0330

    add function

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function factorial(number) {
+  let result = 1;
+
+  for (let i = 0; i < number; i++) {
+    result = result * (number - i);
     
-
-
+  }
+  return result
 }
 
-console.log(factorial(3)); 
+console.log(factorial(3));
 // 3*2*1 = 6
 
 console.log(factorial(4));
